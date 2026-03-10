c321512767ef627d25f2181312c426dd79bb6879
Herun Tarun <herun.s@gmail.com>
2026-03-10 16:37:57 -0400

    added scoreboard for games up to five points

--- lesson1/rps_game.py
+++ lesson1/rps_game.py
@@ -1,5 +1,25 @@
 import random
 
+class Scoreboard:
+    GAMES_TO_WIN = 5
+    def __init__(self):
+        self.score = {'player': 0, 'computer': 0}
+
+    @property
+    def values(self):
+        return self.score.values()
+    
+    def add_points(self, player, points):
+        self.score[player] += points
+
+    def current_score(self):
+        print(f'The score is {self.score['player']} : {self.score['computer']}')
+    
+    def _reset(self):
+        self.score['player'] = 0
+        self.score['computer'] = 0
+
+
 class Player:
     CHOICES = ('rock', 'paper', 'scissors')
     def __init__(self):
@@ -31,19 +51,25 @@ class RPSGame:
     def __init__(self):
         self._human = Human()
         self._computer = Computer()
+        self.scores = Scoreboard()
 
     def play(self):
         self._display_welcome_message()
+        self.scores._reset()
         while True:
             self._human.choose()
             self._computer.choose()
             self._display_winner()
-            if not self._play_again():
-                break
+            self.scores.current_score()
+            if self._is_max_game_length():
+                self.scores._reset()
+                if not self._play_again():
+                    break
         self._display_goodbye_message()
 
     def _display_welcome_message(self):
         print('Welcome to Rock, Paper, Scissors')
+        print('First one to 5 points wins. Good luck!')
 
     def _display_goodbye_message(self):
         print('Thanks for playing Rock, Paper, Scissors. Goodbye!')
@@ -56,8 +82,10 @@ class RPSGame:
         print(f'The computer chose {self._computer.move}')
 
         if self._human_wins(human_move, computer_move):
+            self.scores.add_points('player', 1)
             print('You win!')
         elif self._computer_wins(human_move, computer_move):
+            self.scores.add_points('computer', 1)
             print('You lose!')
         else:
             print("It's a tie!")
@@ -75,6 +103,9 @@ class RPSGame:
     def _play_again(self):
         answer = input('Would you like to play again? (y/n): ')
         return answer.lower().startswith('y')
+    
+    def _is_max_game_length(self):
+        return any([num for num in self.scores.values if num == 5])
 
 
 RPSGame().play()
