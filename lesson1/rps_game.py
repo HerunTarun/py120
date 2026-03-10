fef4d05b95ec19a4dedb54920baba527cad7d5df
Herun Tarun <herun.s@gmail.com>
2026-03-10 17:11:05 -0400

    added lizard and spock

--- lesson1/rps_game.py
+++ lesson1/rps_game.py
@@ -4,10 +4,6 @@ class Scoreboard:
     GAMES_TO_WIN = 5
     def __init__(self):
         self.score = {'player': 0, 'computer': 0}
-
-    @property
-    def values(self):
-        return self.score.values()
     
     def add_points(self, player, points):
         self.score[player] += points
@@ -21,7 +17,7 @@ class Scoreboard:
 
 
 class Player:
-    CHOICES = ('rock', 'paper', 'scissors')
+    CHOICES = ('rock', 'paper', 'scissors', 'lizard', 'spock')
     def __init__(self):
         self.move = None
 
@@ -37,7 +33,7 @@ class Human(Player):
         super().__init__()
 
     def choose(self):
-        prompt = 'Pick rock, paper, or scissors: '
+        prompt = 'Pick rock, paper, scissors, lizard, spock: '
         while True:
             choice = input(prompt).lower()
             if choice in Player.CHOICES:
@@ -48,6 +44,11 @@ class Human(Player):
         self.move = choice
 
 class RPSGame:
+    WINNING_COMBINATIONS = {'rock': ['scissors', 'lizard'],
+                        'scissors': ['paper', 'lizard'],
+                        'paper': ['rock', 'spock'],
+                        'lizard': ['spock', 'paper'],
+                        'spock': ['scissors', 'rock']}
     def __init__(self):
         self._human = Human()
         self._computer = Computer()
@@ -63,16 +64,17 @@ class RPSGame:
             self.scores.current_score()
             if self._is_max_game_length():
                 self.scores._reset()
+                self._display_match_winner()
                 if not self._play_again():
                     break
         self._display_goodbye_message()
 
     def _display_welcome_message(self):
-        print('Welcome to Rock, Paper, Scissors')
+        print('Welcome to Rock, Paper, Scissors, Lizard, Spock')
         print('First one to 5 points wins. Good luck!')
 
     def _display_goodbye_message(self):
-        print('Thanks for playing Rock, Paper, Scissors. Goodbye!')
+        print('Thanks for playing Rock, Paper, Scissors, Lizard, Spock. Goodbye!')
 
     def _display_winner(self):
         human_move = self._human.move
@@ -90,22 +92,26 @@ class RPSGame:
         else:
             print("It's a tie!")
 
+    def _display_match_winner(self):
+        print(f"That's the match! {self._match_winner()}")
+
     def _human_wins(self, human_move, computer_move):
-        return ((human_move == 'rock' and computer_move == 'scissors') or
-            (human_move == 'paper' and computer_move == 'rock') or
-            (human_move == 'scissors' and computer_move == 'paper'))
+        return computer_move in self.WINNING_COMBINATIONS[human_move]
 
     def _computer_wins(self, human_move, computer_move):
-        return ((human_move == 'rock' and computer_move == 'paper') or
-              (human_move == 'paper' and computer_move == 'scissors') or
-              (human_move == 'scissors' and computer_move == 'rock'))
+        return human_move in self.WINNING_COMBINATIONS[computer_move]
 
     def _play_again(self):
         answer = input('Would you like to play again? (y/n): ')
         return answer.lower().startswith('y')
     
+    def _match_winner(self):
+        if self.scores.score['player'] == 5:
+            return 'You win!'
+        return 'Computer wins!'
+    
     def _is_max_game_length(self):
-        return any([num for num in self.scores.values if num == 5])
+        return any([num for num in self.scores.score.values() if num == 5])
 
 
 RPSGame().play()
