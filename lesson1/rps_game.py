6a88b0f5fbc094d8cadc3e942a10e852c0c9613f
Herun Tarun <herun.s@gmail.com>
2026-03-11 16:36:40 -0400

    fixed for pylint

--- lesson1/rps_game.py
+++ lesson1/rps_game.py
@@ -5,7 +5,7 @@ class Scoreboard:
     GAMES_TO_WIN = 5
     def __init__(self):
         self.score = {'player': 0, 'computer': 0}
-    
+
     def add_points(self, player, points):
         self.score[player] += points
 
@@ -13,81 +13,82 @@ class Scoreboard:
         print(messages['current_score'].format(
             player=self.score['player'],
             computer=self.score['computer']))
-    
-    def _reset(self):
+
+    def reset(self):
         self.score['player'] = 0
         self.score['computer'] = 0
 
 class Move:
     def __init__(self):
         self._name = type(self).__name__.lower()
-    
+
     def display(self, identity):
         if identity == 'human':
             return messages['human_choice'].format(move=self._name)
-        
+
         return messages['computer_choice'].format(move=self._name)
 
     def __str__(self):
-        return self.name
-    
+        return self._name
+
 class Rock(Move):
     def __init__(self):
         super().__init__()
         self._wins_against = [Scissors, Lizard]
-    
+
     def __gt__(self, other):
         if not isinstance(other, Move):
             return NotImplemented
-        
+
         return type(other) in self._wins_against
 
 class Paper(Move):
     def __init__(self):
         super().__init__()
         self._wins_against = [Rock, Spock]
-    
+
     def __gt__(self, other):
         if not isinstance(other, Move):
             return NotImplemented
-        
+
         return type(other) in self._wins_against
 
 class Scissors(Move):
     def __init__(self):
         super().__init__()
         self._wins_against = [Paper, Lizard]
-    
+
     def __gt__(self, other):
         if not isinstance(other, Move):
             return NotImplemented
-        
+
         return type(other) in self._wins_against
 
 class Lizard(Move):
     def __init__(self):
         super().__init__()
         self._wins_against = [Paper, Spock]
-    
+
     def __gt__(self, other):
         if not isinstance(other, Move):
             return NotImplemented
-        
+
         return type(other) in self._wins_against
 
 class Spock(Move):
     def __init__(self):
         super().__init__()
         self._wins_against = [Rock, Scissors]
-    
+
     def __gt__(self, other):
         if not isinstance(other, Move):
             return NotImplemented
-        
+
         return type(other) in self._wins_against
 
 class Player:
-    CHOICES = ('rock', 'paper', 'scissors', 'lizard', 'spock', 'r', 'p', 's', 'l', 'sp')
+    CHOICES = ('rock', 'paper', 'scissors', 'lizard', 'spock',
+               'r', 'p', 's', 'l', 'sp')
     def __init__(self):
         self.move = None
 
@@ -133,7 +134,7 @@ class RPSGame:
              'paper': Paper(),
              'lizard': Lizard(),
              'spock': Spock()}
-    
+
     def __init__(self):
         self._human = Human()
         self._computer = Computer()
@@ -141,14 +142,14 @@ class RPSGame:
 
     def play(self):
         self._display_welcome_message()
-        self.scores._reset()
+        self.scores.reset()
         while True:
             self._human.choose()
             self._computer.choose()
             self._display_winner()
             self.scores.current_score()
             if self._is_max_game_length():
-                self.scores._reset()
+                self.scores.reset()
                 self._display_match_winner()
                 if not self._play_again():
                     break
@@ -180,23 +181,17 @@ class RPSGame:
     def _display_match_winner(self):
         print(messages['match_end'].format(match_winner=self._match_winner()))
 
-    def _human_wins(self, human_move, computer_move):
-        return computer_move in self.WINNING_COMBINATIONS[human_move]
-
-    def _computer_wins(self, human_move, computer_move):
-        return human_move in self.WINNING_COMBINATIONS[computer_move]
-
     def _play_again(self):
         answer = input(messages['play_again'])
         return answer.lower().startswith('y')
-    
+
     def _match_winner(self):
         if self.scores.score['player'] == Scoreboard.GAMES_TO_WIN:
             return messages['win']
         return messages['computer_win']
-    
+
     def _is_max_game_length(self):
-        return any([num for num in self.scores.score.values() 
+        return any([num for num in self.scores.score.values()
                     if num == Scoreboard.GAMES_TO_WIN])
 
 with open('rps_game.json', 'r') as file:
