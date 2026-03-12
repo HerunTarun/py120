60c85a6ff412ba7aef1f08c8601ba59ba939cce7
Herun Tarun <herun.s@gmail.com>
2026-03-12 18:35:43 -0400

    converted global json messages to local self.messages inside RPSGame class

--- lesson1/rps_game.py
+++ lesson1/rps_game.py
@@ -12,7 +12,7 @@ class Scoreboard:
         self.score[player] += points
 
     def current_score(self):
-        print(messages['current_score'].format(
+        print(game.messages['current_score'].format(
             player=self.score['player'],
             computer=self.score['computer']))
 
@@ -28,13 +28,13 @@ class Scoreboard:
 
     def query_history(self):
         while True:
-            lines = input(messages['history_query'])
+            lines = input(game.messages['history_query'])
             if lines.startswith('a'):
                 return len(self.history)
             try:
                 lines = int(lines)
             except ValueError:
-                print(messages['invalid_help'])
+                print(game.messages['invalid_help'])
                 continue
 
             if lines > len(self.history):
@@ -44,10 +44,11 @@ class Scoreboard:
 
     def display_history(self, lines):
         if not lines:
-            print(messages['empty_history'])
+            print(game.messages['empty_history'])
 
         for i in range(lines):
-            print(messages['display_history'].format(round=self.history[i]))
+            print(game.messages['display_history'].format(
+                                                    round=self.history[i]))
 
 class Move:
     def __init__(self):
@@ -55,9 +56,9 @@ class Move:
 
     def display(self, identity):
         if identity == 'human':
-            return messages['human_choice'].format(move=self._name)
+            return game.messages['human_choice'].format(move=self._name)
 
-        return messages['computer_choice'].format(computer=identity,
+        return game.messages['computer_choice'].format(computer=identity,
                                                   move=self._name)
 
     def __str__(self):
@@ -150,7 +151,7 @@ class HAL(Computer):
         self.name = 'HAL'
 
     def choose_move(self):
-        if random.choice(range(100)) > self.SCISSOR_CHANCE:
+        if random.choice(range(100)) < self.SCISSOR_CHANCE:
             self.move = Scissors()
         else:
             self.move = random.choice(list(RPSGame.MOVES.values()))
@@ -174,7 +175,7 @@ class Human(Player):
 
     def choose_move(self):
         while True:
-            choice = input(messages['choose_move']).lower()
+            choice = input(game.messages['choose_move']).lower()
             if choice in Player.OPTIONS:
                 self._identify_option(choice)
                 continue
@@ -182,13 +183,13 @@ class Human(Player):
                 choice = self._format_choice(choice)
                 break
 
-            print(messages['invalid_choice'])
+            print(game.messages['invalid_choice'])
 
         self.move = RPSGame.MOVES[choice]
 
     def choose_opponent(self):
         while True:
-            opponent = input(messages['choose_opponent']).lower()
+            opponent = input(game.messages['choose_opponent']).lower()
             if opponent in Player.OPTIONS:
                 self._identify_option(opponent)
                 continue
@@ -196,7 +197,7 @@ class Human(Player):
             if opponent in Player.OPPONENT_CHOICES:
                 opponent = self._format_opponent(opponent)
                 return game.opponents[opponent]
-            print(messages['invalid_opponent'])
+            print(game.messages['invalid_opponent'])
 
     def _identify_option(self, option):
         history = ['his', 'history']
@@ -236,6 +237,8 @@ class RPSGame:
              'spock': Spock()}
 
     def __init__(self):
+        with open('rps_game.json', 'r') as file:
+            self.messages = json.load(file)
         self.scores = Scoreboard()
         self._human = Human(self.scores)
         self._computer = None
@@ -276,20 +279,21 @@ class RPSGame:
 
     def _display_welcome_message(self):
         self._clear_screen()
-        print(messages['welcome'])
-        print(messages['game_rules'])
-        print(messages['point_system'])
-        print(messages['history_intro'])
+        print(self.messages['welcome'])
+        print(self.messages['game_rules'])
+        print(self.messages['point_system'])
+        print(self.messages['history_intro'])
 
     def _display_goodbye_message(self):
-        print(messages['goodbye'])
+        print(self.messages['goodbye'])
 
     def _display_choices(self):
         print(self._human.move.display('human'))
         print(self._computer.move.display(f'{self._computer.name}'))
 
     def _display_opponent(self):
-        print(messages['opponent_intro'].format(opponent=self._computer.name))
+        print(self.messages['opponent_intro'].format(
+                                                opponent=self._computer.name))
     def _calculate_winner(self):
         human_move = self._human.move
         computer_move = self._computer.move
@@ -315,33 +319,34 @@ class RPSGame:
     def _display_winner(self, winner):
         match winner:
             case 'Player wins!':
-                print(messages['win'])
+                print(self.messages['win'])
             case 'Computer wins!':
-                print(messages['lose'])
+                print(self.messages['lose'])
             case "It's a draw!":
-                print(messages['tie'])
+                print(self.messages['tie'])
 
     def _display_match_winner(self):
-        print(messages['match_end'].format(match_winner=self._match_winner()))
+        print(self.messages['match_end'].format(
+                                            match_winner=self._match_winner()))
 
     def _clear_screen(self):
         os.system('clear')
 
     def _play_again(self):
-        answer = input(messages['play_again'])
+        answer = input(self.messages['play_again'])
         return answer.lower().startswith('y')
 
     def _match_winner(self):
         if self.scores.score['player'] >= Scoreboard.GAMES_TO_WIN:
-            return messages['win']
-        return messages['computer_win'].format(computer=self._computer.name)
+            return self.messages['win']
+        return self.messages['computer_win'].format(
+                                                computer=self._computer.name)
 
     def _is_max_game_length(self):
         return any([num for num in self.scores.score.values()
                     if num >= Scoreboard.GAMES_TO_WIN])
 
-with open('rps_game.json', 'r') as file:
-    messages = json.load(file)
+
 
 game = RPSGame()
 
